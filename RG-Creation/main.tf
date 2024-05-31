e41a1056169ccbecfc03a3b15401920b0e225a2d
Ramaraju <103272173+Ramaraju-devops@users.noreply.github.com>
2024-05-31 11:21:37 +0530

    Update main.tf

--- RG-Creation/main.tf
+++ RG-Creation/main.tf
@@ -34,9 +34,9 @@ terraform {
 # 2. Terraform Provider Block for AzureRM
 provider "azurerm" {
   features {}
-   client_id = "5eaded78-69b1-4513-8de9-cac216967684"
-   client_secret = "NpJ8Q~yvTYkZrUdAnKv85OOr_GiOfshXq3zFDa0N"
-   tenant_id = "84f96efe-d3ca-47a5-bc04-8d455c1e0e15"
-   subscription_id = "13917d14-a7e0-4435-8830-403a28246744" 
+#   client_id = "5eaded78-69b1-4513-8de9-cac216967684"
+#   client_secret = "NpJ8Q~yvTYkZrUdAnKv85OOr_GiOfshXq3zFDa0N"
+#   tenant_id = "84f96efe-d3ca-47a5-bc04-8d455c1e0e15"
+#   subscription_id = "13917d14-a7e0-4435-8830-403a28246744" 
 }
 
