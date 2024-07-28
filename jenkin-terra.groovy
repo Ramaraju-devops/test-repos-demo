dd6396e6b718ba9e7d877a22e2dfa5f14846586e
Ramaraju <103272173+Ramaraju-devops@users.noreply.github.com>
2024-07-28 10:37:18 +0530

    Update jenkin-terra.groovy

--- jenkin-terra.groovy
+++ jenkin-terra.groovy
@@ -2,19 +2,42 @@ pipeline {
     agent any
 
     environment {
-        AZURE_CREDENTIALS = credentials('08f79609-f178-4576-8efd-f76e173dc691')
-        TF_VAR_client_id = "${AZURE_CREDENTIALS_USR}"
-        TF_VAR_client_secret = "${AZURE_CREDENTIALS_PSW}"
-        TF_VAR_subscription_id = "${AZURE_CREDENTIALS_SUBSCRIPTION_ID}"
-        TF_VAR_tenant_id = "${AZURE_CREDENTIALS_TENANT}"
+        AZURE_CREDENTIALS = credentials('AzureServicePrincipal2')
+        ARM_CLIENT_ID = credentials('azure-client-id')
+        ARM_CLIENT_SECRET = credentials('azure-client-secret')
+        ARM_SUBSCRIPTION_ID = credentials('azure-subscription-id')
+        ARM_TENANT_ID = credentials('azure-tenant-id')
     }
 
     stages {
-        // stage('Checkout') {
-        //     steps {
-        //         git checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'd4c79c4f-4624-4fdb-9eef-ed30370e7d07', url: 'https://github.com/Ramaraju-devops/test-repos-demo']])
+        stage('Checkout') {
+            steps {
+               script {
+                    git branch: 'master',
+                        credentialsId: 'Github-connection',
+                        url: 'https://github.com/Ramaraju-devops/test-repos-demo' 
+               }
+        }
+        }
+        
+        // stage('az login'){
+        //     steps{
+        //     azureCLI commands: [[exportVariablesString: '', script: 'az login --service-principal -u $AZURE_CREDENTIALS_CLIENT_ID -p $AZURE_CREDENTIALS_CLIENT_SECRET --tenant $AZURE_CREDENTIALS_TENANT_ID'], [exportVariablesString: '', script: '']], principalCredentialId: 'AzureServicePrincipal2'
         //     }
         // }
+
+        // working block
+        // stage('testing') {
+        //     steps {
+        //        script {
+        //             sh '''az login --service-principal -u $AZURE_CREDENTIALS_CLIENT_ID -p $AZURE_CREDENTIALS_CLIENT_SECRET --tenant $AZURE_CREDENTIALS_TENANT_ID
+        //                   az account show
+        //                   cd RG-Creation
+        //                   terraform init'''
+        //        }
+        // }
+        // }
+    
 
         stage('Terraform Init') {
             steps {
@@ -44,3 +67,4 @@ pipeline {
         }
     }
 }
+
