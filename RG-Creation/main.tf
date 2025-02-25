84c74268d92999875ee4bd24b6fe0726138c5aa7
Ramaraju <103272173+Ramaraju-devops@users.noreply.github.com>
2025-02-25 12:07:28 +0530

    Update main.tf

--- RG-Creation/main.tf
+++ RG-Creation/main.tf
@@ -22,12 +22,7 @@ terraform {
 
 # Terraform State Storage to Azure Storage Container
 
-  backend "azurerm" {
-    resource_group_name   = "github-tfstates-rg"
-    storage_account_name  = "stracctgithubterr01"
-    container_name        = "tfstatecontainer"
-    key                   = "jenkinspipe.tfstate"
-  } 
+
 }
 
 
