e5d2ef18e1d4ee7e95d7889c4cdbe7c3d4f751a4
Ramaraju <ramavenkat5155@gmail.com>
2025-04-11 10:10:21 +0530

    tf code update 3

--- RG-Creation/provider.tf
+++ RG-Creation/provider.tf
@@ -15,5 +15,6 @@ provider "azurerm" {
   features {}
    subscription_id = "13917d14-a7e0-4435-8830-403a28246744"
    tenant_id = "84f96efe-d3ca-47a5-bc04-8d455c1e0e15"
+   client_id = "7a6ff7d1-7f27-4fa9-8d0a-46b052bda94f"
 }
 
